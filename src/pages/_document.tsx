5c2764963c7d19be1f65181e1cafddeaac910ec6
RameshCentaurInteractive <ramesh.k@centaurinteractive.com>
2024-04-01 12:50:22 +0530

    [fix] added the viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Script src="https://aframe.io/releases/1.5.0/aframe.min.js" strategy="beforeInteractive" />
       <Script src="https://unpkg.com/mind-ar@latest/dist/mindar-face-aframe.prod.js" strategy="beforeInteractive" />
       <Script src="https://raw.githubusercontent.com/mayognaise/aframe-screenshot-component/master/dist/aframe-screenshot-component.min.js"/>
